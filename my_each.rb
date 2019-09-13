6c8367ee208f40fb64a4f240eca5e25ecb049f2f
Alyssa Yates <bosslys@icloud.com>
2019-09-13 18:53:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,5 @@
-def my_each # put argument(s) here
-  # code here
+collection = ["zebras", "horses", "monkeys", "dogs"]
+
+def my_each(collection) do |i| # put argument(s) here
+  puts i # code here
 end
